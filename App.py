2943eae2c2215f7ae91afb77716e417f6bb7166e
irhassha <irhassha@gmail.com>
2025-05-07 00:06:36 +0700

    Update App.py
    tambah area remark

--- App.py
+++ App.py
@@ -125,4 +125,13 @@ for col, prefix in zip(cols, prefixes):
                 legend_title='Carrier Out',
                 margin=dict(t=10, b=10, l=10, r=10), height=260
             )
+                        # Tambahkan keterangan Area di bawah legend
+            fig.add_annotation(
+                text=area,
+                xref='paper', yref='paper',
+                x=1.02, y=0.15,
+                showarrow=False,
+                font=dict(size=14, color='lightgray'),
+                align='center'
+            )
             st.plotly_chart(fig, use_container_width=True)
